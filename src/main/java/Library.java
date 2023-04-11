d3631aa9d421a733892672a42a28b0c9394d9f5a
Chris Barclay <chris.barclay1@gmail.com>
2023-04-11 17:30:02 +0100

    Third Commit - MVP Reached

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> inventory;
+
+    public Library(){
+        this.inventory = new ArrayList<>();
+    }
+
+    public int numOfBooksInInventory(){
+        return this.inventory.size();
+    }
+
+    public void addBookToInventory(Book book) {
+        this.inventory.add(book);
+    }
+
+    public boolean searchInventory(String title) {
+        for(Book book : this.inventory){
+            if(book.getTitle().equals(title)){
+                return true;
+            }
+        } return false;
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    private Book book;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+    private Book book5;
+
+    @Before
+    public void setup(){
+        library = new Library();
+        book = new Book("The Shining", "Stephen King", "Horror");
+        book2 = new Book("The Institute", "Stephen King", "Horror");
+        book3 = new Book("11/22/63", "Stephen King", "Science Fiction");
+        book4 = new Book("Under the Skin", "Michael Faber", "Science Fiction");
+        book5 = new Book("The Bone Clocks", "David Mitchell", "Science Fiction");
+        library.addBookToInventory(book);
+        library.addBookToInventory(book2);
+        library.addBookToInventory(book3);
+        library.addBookToInventory(book4);
+        library.addBookToInventory(book5);
+    }
+
+    @Test
+    public void canCountBooksInInventory(){
+        assertEquals(5, library.numOfBooksInInventory());
+    }
+
+    @Test
+    public void canAddBookToInventory(){
+        Book book = new Book("The Shining", "Stephen King", "Horror");
+        library.addBookToInventory(book);
+        assertEquals(6, library.numOfBooksInInventory());
+    }
+
+    @Test
+    public void canSearchForTitleTrue(){
+        assertEquals(true, library.searchInventory("The Institute"));
+    }
+    @Test
+    public void canSearchForTitleFalse(){
+        assertEquals(false, library.searchInventory("The Stand"));
+    }
 }
